06f69bbb5def424a52bca4840811261880a07f76
zachshouts <117788174+zachshouts@users.noreply.github.com>
2023-02-17 09:42:01 -0600

    Updated file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html',
+  content: ['./index.html',
     './public/**/**/*.{js}',
     './public/**/*.{html}'],
   theme: {
